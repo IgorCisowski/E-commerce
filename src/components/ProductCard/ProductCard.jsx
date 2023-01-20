9c0ce9ef8f0a7b8b42fc1556be2c3c6b8441c79a
IgorCisowski <igorcisowski2098@gmail.com>
2023-01-20 17:04:31 +0100

    style: changed log anchor to black

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import "./ProductCard.css";
 
+// const CardContainer = styled.div`
+//   margin-top: 40px;
+//   width: 100%;
+//   display: flex;
+//   gap: 4%;
+//   color: ${({isActive}) => isActive ? '' : ''}
+// `
+
 export const ProductCard = ({ products }) => {
   return (
-    <>
-      <div className="cardContainer">
-        <div className="card">
-          <img src={products.image} alt="" />
-          <div className="cardDescription">
-            <p>{products.tile}</p>
-            <span>£{products.price}</span>
-          </div>
+    // <CardContainer isActive={true}>
+    <div className="cardContainer">
+      <div className="card">
+        <img src={products.image} alt="" />
+        <div className="cardDescription">
+          <p>{products.tile}</p>
+          <span>£{products.price}</span>
         </div>
       </div>
-    </>
+    </div>
+    // </CardContainer>
   );
 };
